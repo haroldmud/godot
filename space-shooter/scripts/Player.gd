f4b2065976eabd6179262e6ab6208a3b0e12fdd6
haroldmud <haroldmudosa@gmail.com>
2026-01-19 16:48:59 +0200

    feat: implement the direction vectors actions

--- space-shooter/scripts/Player.gd
+++ space-shooter/scripts/Player.gd
@@ -4,16 +4,14 @@ const SPEED = 64.0
 
 var direction := Vector2.RIGHT
 
-@export var game_player: Node2D
+@onready var player: Node2D = $"."
+@onready var meteor: Node2D = $"../Meteor"
 
-@onready var meteor := me
-
-
-# Called when the node enters the scene tree for the first time.
+# we won't need this since we already have teh @onready annotations.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass 
+	var input_direaction := Input.get_vector("move_left", "move_right", "move_forward", "move_backward") 
