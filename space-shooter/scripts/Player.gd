fd2eea9b2cab24910df71e5afcc8c04c49ff8c5e
haroldmud <haroldmudosa@gmail.com>
2026-01-20 16:05:36 +0200

    feat: implement some changes in the _ready function

--- space-shooter/scripts/Player.gd
+++ space-shooter/scripts/Player.gd
@@ -6,7 +6,6 @@ var direction := Vector2.RIGHT
 
 @onready var player: Node2D = $"."
 @onready var meteor: Node2D = $"../Meteor"
-
 # we won't need this since we already have teh @onready annotations.
 func _ready() -> void:
 	position = Vector2(100, 200)
