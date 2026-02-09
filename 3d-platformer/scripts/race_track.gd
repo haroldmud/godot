040923d4147354901c6fb9a10ff360d3b0c3fdb6
haroldmud <haroldmudosa@gmail.com>
2026-02-09 13:27:57 +0200

    feat: implementing the collision layers tho it didn't work

--- 3d-platformer/scripts/race_track.gd
+++ 3d-platformer/scripts/race_track.gd
@@ -5,4 +5,4 @@ func _on_area_3d_body_entered(body: Node3D) -> void:
 	print("a collision surely happened")
 	if body is RigidBody3D:
 		body.linear_velocity = Vector3.ZERO
-		body.global_position = body.global_position - body.transform.basis.y * 5
+		body.global_position = body.global_position - body.transform.basis.x * 5
