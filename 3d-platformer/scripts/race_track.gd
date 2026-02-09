be62eb6921a1ad52764b8277c2b0fa2d0b9e25d1
haroldmud <haroldmudosa@gmail.com>
2026-02-09 13:20:26 +0200

    feat: implement collisions to the environmen

---
+++ 3d-platformer/scripts/race_track.gd
@@ -0,0 +1,8 @@
+extends MeshInstance3D
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	print("a collision surely happened")
+	if body is RigidBody3D:
+		body.linear_velocity = Vector3.ZERO
+		body.global_position = body.global_position - body.transform.basis.y * 5
